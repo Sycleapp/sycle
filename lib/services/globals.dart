a4e19255be4c70d3ae278d229c8d0b01605036cb
Chase Steele <36288556+CHASESTEELE@users.noreply.github.com>
2020-03-31 18:01:39 -0700

    Update 3/31/20

--- lib/services/globals.dart
+++ lib/services/globals.dart
@@ -5,9 +5,20 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 /// Static global state. Immutable services that do not care about build context. 
 class Global {
   // App Data
-  static final String title = 'Sycle';
+  static final String title = 'Fireship';
 
   // Services
   static final FirebaseAnalytics analytics = FirebaseAnalytics();
+
+    // Data Models
+  static final Map models = {
+    User: (data) => User.fromMap(data),
+    Stories: (data) => Stories.fromMap(data),
+    Reactions: (data) => Reactions.fromMap(data),
+  };
+
+  // Firestore References for Writes
+  static final Collection<Reactions> topicsRef = Collection<Reactions>(path: 'reactions');
+
   
 }
